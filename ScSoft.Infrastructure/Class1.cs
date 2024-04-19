e5b4563cfafed4a233423ebda653c7cce82acd87
Jarosław Jaśkiewicz <jarek@hkr.net.pl>
2024-04-19 17:15:24 +0200

    Dodaj pliki projektów.

---
+++ ScSoft.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ScSoft.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ ScSoft.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScSoft.Infrastructure;
+
+    public static class DependencyInjection
+    {
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+    //public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+    //{
+    //    return app;
+    //}
+}
+
